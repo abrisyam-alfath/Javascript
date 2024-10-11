ebd460d49b2571d2a9be744582e811e13a1419b0
abrisyam-alfath <m.abrisyam@student.alhazen.academy>
2024-10-11 08:53:24 +0700

    Add files via upload

---
+++ js introduction/script.js
@@ -0,0 +1,6 @@
+// alert= sebuah function untuk menampilkan pop up
+alert("selmata datang");
+alert("ini adalah alert")
+
+// prompt= sebuah function untuk menerima input dari user
+prompt("siapa nama kamu ?")
