8989b8c3134e2ec026d4ded7d9d85b3e955c3541
abrisyam-alfath <m.abrisyam@student.alhazen.academy>
2024-10-18 14:07:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+// alert= sebuah function untuk menampilkan pop up
+// alert("selmata datang");
+// alert("ini adalah alert")
+
+// prompt= sebuah function untuk menerima input dari user
+// prompt("siapa nama kamu ?")
+
+// variable var
+// deskripsi awal
+var nama = "ahmad";
+console.log(nama);
+// diubah
+nama = "budi"
+console.log(nama);
+// deskripsi ulang
+var nama = "asep"
+console.log(nama);
+
+// variable let
+// deskripsi awal
+let umur = 20;
+console.log(umur);
+// // diubah
+umur = 31
+console.log(umur);
+// tidak bisa deskripsi ulang
+// let umur = "25"
+// console.log(umur);
+
+// variable const
+// deskripsi awal
+const alamat = "bogor";
+console.log(alamat);
+// // tidak bisa diubah
+// alamat = "depok"
+// console.log(alamat);
+// tidak bisa deskripsi ulang
+// let umur = "25"
+// console.log(umur);
+
+
+// calculator
+
+// console.log(6+2);
+
+// angka1 = 13;
+// angka2 = 10;
+// hasil = angka1 + angka2;
+// console.log(hasil);
+
+//membuat calculator sederhana
+let number1 = prompt("masukan angka pertama : ");
+let number2 = prompt("masukan angka kedua : ");
+let result = parseInt(number1) + parseInt(number2);
+alert(result);
+
+
+// array
+fruits=["banana","apple","orange","durian"];
+
+alert(fruits[2]);
+
+
+
